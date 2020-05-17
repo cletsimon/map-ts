be4a66dd41bafbbf72c1a95740ae276d211dca57
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-17 17:20:04 +0800

    bad implementation

--- README.md
+++ README.md
@@ -10,6 +10,13 @@ npm install faker
 npm install @types/faker
 ```
 
+# Maps Setup
+
+1. Generate a google dev project at http://console.developers.google.com
+2. Enable goole maps support inside the project
+3. Generate an API key
+4. Add the google maps script tag to our HTML file
+
 ## running parcel
 
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
-console.log(user);
-
 const company = new Company();
-console.log(company);
+const customMap = new CustomMap('map');
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
