53fa7082133e2d796d10bdcc5b3b95e61e738fa7
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-17 17:34:06 +0800

    best approach to for addMarker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,11 @@
-import { User } from './User';
-import { Company } from './Company';
+// instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,9 +20,7 @@ export class CustomMap {
     });
   }
 
-  // not a good approach
-  // if we keep adding different classes that are mappable
-  addMarker(mappable: User | Company): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
@@ -25,14 +29,4 @@ export class CustomMap {
       }
     });
   }
-
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng
-  //     }
-  //   });
-  // }
 }
